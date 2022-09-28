2b550112606f5d81acae0dcb0a711c8e274a59e9
Paul Thomas <paulthomas2107@gmail.com>
2022-09-28 16:06:08 +0100

    More....

--- cursesddemo.py
+++ cursesddemo.py
@@ -1,1 +1,14 @@
 #  I am a comment
+import curses
+from curses import wrapper
+
+
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr(10, 10, "Hello Paul !", curses.A_UNDERLINE  )
+    stdscr.addstr(15, 25, "Hello Caitlin !")
+    stdscr.refresh()
+    stdscr.getch()
+
+
+wrapper(main)
